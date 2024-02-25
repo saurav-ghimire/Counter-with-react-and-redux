31e7dc7e707fafb8aaa89f5117fdb63978bddea5
saurav-ghimire <sauravghimire0123@gmail.com>
2024-02-24 20:18:20 -0500

    Bulk Value increasing and decreasing feature added and used useRef hooks to handle it

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,6 +1,11 @@
+import { useRef } from "react";
 import { useDispatch } from "react-redux";
 
 function Controls() {
+
+  const adding = useRef();
+  const subtract = useRef();
+  
   const dispatch = useDispatch();
 
   const handleIncreament = () => {
@@ -11,10 +16,46 @@ function Controls() {
     dispatch({ type : 'DECREMENT' })
   }
 
+  const handleAdd = () => {
+    dispatch({
+      type : 'ADDING',
+      payload : {
+        addingValue : parseInt(adding.current.value)
+      } 
+    })
+  }
+
+  const handlesubtract = () => {
+    dispatch({
+      type : 'SUBSTRACT',
+      payload : {
+        subtractValue : parseInt(subtract.current.value)
+      } 
+    })
+  }
+
   return <>
   <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
     <button type="button" className="btn btn-primary btn-lg px-4 gap-3" onClick={handleIncreament}>Increase</button>
     <button type="button" className="btn btn-outline-secondary btn-lg px-4" onClick={handleDecrement}>Decrease</button>
+  </div>
+  <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mt-2">
+    <input
+    type="number"
+    placeholder="Enter Numbers"
+    className="px-2"
+    ref={adding}
+    />
+    <button type="button" className="btn btn-outline-secondary px-4" onClick={handleAdd}>Add</button>
+  </div>
+  <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mt-2">
+    <input
+    type="number"
+    placeholder="Enter Numbers"
+    className="px-2"
+    ref={subtract}
+    />
+    <button type="button" className="btn btn-outline-secondary px-4" onClick={handlesubtract}>Subtract</button>
   </div>
   </>;
 }
--- src/store/store.jsx
+++ src/store/store.jsx
@@ -10,6 +10,13 @@ const counterReducer = (store = INITIAL_VALUE, action) => {
   }else if(action.type === 'DECREMENT'){
     return {counter : store.counter - 1}
   }
+  else if(action.type === 'ADDING'){
+    return {counter : store.counter + action.payload.addingValue}
+  }
+
+  else if(action.type === 'SUBSTRACT'){
+    return {counter : store.counter - action.payload.subtractValue}
+  }
   return store;
 }
 const counterStore = createStore(counterReducer);
